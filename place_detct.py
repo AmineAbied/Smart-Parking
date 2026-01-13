3147fc9614a7eb13971a88270c7f43717deacbe7
Mohamed Amine ABIED <amine_abied@outlook.com>
2026-01-13 21:20:14 +0100

    Brodcasting Empty Spots

--- place_detct.py
+++ place_detct.py
@@ -1,13 +1,13 @@
-import cv2
-import numpy as np
+import cv2  # type: ignore
+import numpy as np  # type: ignore
 
 # Parking spots
 spot1 = (865, 152, 183, 164)
 spot2 = (887, 371, 154, 198)
 spot3 = (880, 576, 154, 176)
 spot4 = (881, 764, 150, 150)
-spot5 = (509, 134, 174, 179)
-spot6 = (705, 145, 133, 166)
+spot6 = (509, 134, 174, 179)
+spot5 = (705, 145, 133, 166)
 spot7 = (4, 578, 156, 170)
 spot8 = (14, 410, 150, 156)
 
@@ -30,6 +30,8 @@ _, diff_thresh = cv2.threshold(diff, 30, 255, cv2.THRESH_BINARY)
 
 parking_spots = [spot1, spot2, spot3, spot4, spot5, spot6, spot7, spot8]
 
+free_spots = []
+
 for i, (x, y, w, h) in enumerate(parking_spots):
     spot_diff = diff_thresh[y:y+h, x:x+w]
     non_zero_count = cv2.countNonZero(spot_diff)
@@ -38,10 +40,29 @@ for i, (x, y, w, h) in enumerate(parking_spots):
     occupancy_ratio = non_zero_count / area
     status = "Occupied" if occupancy_ratio > 0.1 else "Free"
 
+    if status == "Free":
+        free_spots.append(i + 1)
+
     color = (0, 0, 255) if status == "Occupied" else (0, 255, 0)
     cv2.rectangle(current, (x, y), (x+w, y+h), color, 2)
     cv2.putText(current, f"{status}", (x, y - 10),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.6, color, 2)
+
+
+print("Free parking spots: ", free_spots)
+
+from flask import Flask # type: ignore
+
+app = Flask(__name__)
+
+
+@app.route("/parking", methods=["GET"])
+def parking():
+
+    return free_spots
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
 
 
 cv2.imshow("Parking Detection", current)
